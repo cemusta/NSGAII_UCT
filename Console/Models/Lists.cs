487224e8261c031e60784861c40d59b567e6fecd
Kemal Ogün Işık <ogunisik@gmail.com>
2016-02-16 13:37:02 +0200

    Format düzenlemesi

---
+++ Console/Models/Lists.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp.Models {
+    public class Lists {
+        private int index { get; set; }
+        private Lists parent { get; set; }
+        private Lists child { get; set; }
+    }
+}
