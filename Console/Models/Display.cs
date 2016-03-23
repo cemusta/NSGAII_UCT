c1db76c2e0dc20e1b16c20b7779335827712ce5a
Cem Usta <nooneelse@gmail.com>
2016-03-23 16:10:01 +0200

    3d plot best
    Signed-off-by: Cem Usta <nooneelse@gmail.com>

--- Console/Models/Display.cs
+++ Console/Models/Display.cs
@@ -58,7 +58,21 @@ namespace ConsoleApp.Models
                     zr[x] = pop.IndList[x].Obj[GnuplotObjective3 - 1];
                 }
 
+                if (best != null)
+                {
+                    GnuPlot.HoldOn();
+                }
+
                 GnuPlot.SPlot(xr, yr, zr, $"title 'Generation #{genNo} of {problem.GenCount}' with points pointtype 8 lc rgb 'blue'");
+
+                if (best != null)
+                {
+                    var x = new double[] { best.Obj[GnuplotObjective1 - 1] };
+                    var y = new double[] { best.Obj[GnuplotObjective2 - 1] };
+                    var z = new double[] { best.Obj[GnuplotObjective3 - 1] };
+                    GnuPlot.SPlot(x, y, z, "title 'best' with points pointtype 6 lc rgb 'red'");
+                }
+
                 GnuPlot.Set($"xlabel \"obj[{GnuplotObjective1 - 1}]\"");
                 GnuPlot.Set($"ylabel \"obj[{GnuplotObjective2 - 1}]\"");
                 GnuPlot.Set($"zlabel \"obj[{GnuplotObjective3 - 1}]\"");
