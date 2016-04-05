07a106ac510be7524c1502522e1e62ea6944ee8a
cemusta <nooneelse@gmail.com>
2016-04-05 10:39:10 +0300

    sample batch file

---
+++ ConsoleTest/sample batch.bat
@@ -0,0 +1,11 @@
+REM SET WORKING FOLDER
+cd %~dp0%
+echo %~dp0%
+REM START TESTING
+C:\DEV\NSGAII_UCT\ConsoleTest\bin\Debug\consoleTest.exe 0.11 200 10000 0
+C:\DEV\NSGAII_UCT\ConsoleTest\bin\Debug\consoleTest.exe 0.11 200 10000 5
+C:\DEV\NSGAII_UCT\ConsoleTest\bin\Debug\consoleTest.exe 0.11 200 10000 6
+C:\DEV\NSGAII_UCT\ConsoleTest\bin\Debug\consoleTest.exe 0.11 200 10000 7
+C:\DEV\NSGAII_UCT\ConsoleTest\bin\Debug\consoleTest.exe 0.11 200 10000 8
+C:\DEV\NSGAII_UCT\ConsoleTest\bin\Debug\consoleTest.exe 0.11 200 10000 9
+C:\DEV\NSGAII_UCT\ConsoleTest\bin\Debug\consoleTest.exe 0.11 200 10000 10
